19088fe7e94497a9b5423535d1c8aeee4169b486
gamussa <gamussa@gmail.com>
2012-05-14 10:35:34 -0400

    Spring webJars for Ext JS
    Simple MVC Demo of integration spring, webJars and Ext JS

--- src/main/resources/assets/app.js
+++ src/main/resources/assets/app.js
@@ -1,7 +1,14 @@
-Ext.application({
-    name:'app',
-    autoCreateViewport: false,
+Ext.Loader.setConfig({
+
+    enabled:true
+});
+
+Ext.create('Ext.app.Application', {
+    name:'USERS',
+    autoCreateViewport:false,
+    controllers:['Users'],
+
     launch:function () {
-        console.log("Hello from Intellijey and ExtJS " + Ext.getVersion('extjs'));
+        console.log("Simple Ext JS mvc application with webjars, spring and Ext JS " + Ext.getVersion('extjs'));
     }
 });
---
+++ src/main/resources/assets/app/model/User.js
@@ -0,0 +1,4 @@
+Ext.define('USERS.model.User', {
+    extend:'Ext.data.Model',
+    fields:['id', 'name', 'email']
+});
